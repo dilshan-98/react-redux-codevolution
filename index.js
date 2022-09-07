2dc31aede89ee033f80956df346702940162dcf3
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2022-09-08 00:04:52 +0530

    info update 2

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const BUY_CAKE = "BUY_CAKE";
 // }
 
 //Action
+//Defining the action as a fucntion makes it easier to add more properties later
 function buyCake() {
     return {
         type: BUY_CAKE,
@@ -53,14 +54,14 @@ console.log('Initial State ', store.getState());  // Output: Initial State  { nu
 
 // 3. Register to the listeners [Subscribe method accepts a function soo..]
 //store.subscribe(() => console.log("Updtaed State ", store.getState()))
-const unsubsribe = store.subscribe(() => console.log("Updtaed State ", store.getState()));
+const unsubsribe = store.subscribe(() => console.log("Updated State ", store.getState()));
 
 // 4. To update the state
 //This usually takes actions directly but since we have a action creater (buyCakes), we can use it here
 //We repeat this just to see the output now
-store.dispatch(buyCake())  // Output: Updtaed State  { numOfCakes: 9 }
-store.dispatch(buyCake())  // Output: Updtaed State  { numOfCakes: 8 }
-store.dispatch(buyCake())  // Output: Updtaed State  { numOfCakes: 7 }
+store.dispatch(buyCake())  // Output: Updated State  { numOfCakes: 9 }
+store.dispatch(buyCake())  // Output: Updated State  { numOfCakes: 8 }
+store.dispatch(buyCake())  // Output: Updated State  { numOfCakes: 7 }
 
-// 5. Unsubsribe the listener [We do it by calling the function returened by the subscribe method]
+// 5. Unsubsribe the listener [We do it by calling the function returned by the subscribe method]
 unsubsribe();
