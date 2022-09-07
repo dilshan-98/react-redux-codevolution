8ddb5ffdae557c563c8e5f0875f69aa25871d4bb
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2022-09-07 22:39:22 +0530

    Reducer

--- index.js
+++ index.js
@@ -13,10 +13,27 @@ const BUY_CAKE = "BUY_CAKE";
 //     info: "First Redux Action"
 // }
 
+//Action
 function buyCake() {
     return {
         type: BUY_CAKE,
         info: "First Redux Action"
+    }
+}
+
+const initialState = {
+    numOfCakes: 10
+}
+
+//Reducer
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case BUY_CAKE: return {
+            ...state, //This is to copy the state first and update only the changing property
+            numOfCakes: state.numOfCakes - 1
+        }
+
+        default: return state
     }
 }
 
