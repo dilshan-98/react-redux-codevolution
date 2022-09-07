b17f561aae3935dec29d6ae051474d7dab7f2ea7
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2022-09-07 22:25:15 +0530

    Action

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 console.log("From index.js")
+
+const BUY_CAKE = "BUY_CAKE";
+
+// Action Object
+// {
+//     type: BUY_CAKE
+// }
+
+// Action object can have properties other than type also
+// {
+//     type: BUY_CAKE,
+//     info: "First Redux Action"
+// }
+
+function buyCake() {
+    return {
+        type: BUY_CAKE,
+        info: "First Redux Action"
+    }
+}
+
