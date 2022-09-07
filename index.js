73c7f251177415240b662b3e201f55d436f5ab06
dilshan-98 <dinusha.dilshan.1.dd@gmail.com>
2022-09-07 23:57:38 +0530

    Store

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
 console.log("From index.js")
 
 const BUY_CAKE = "BUY_CAKE";
@@ -21,6 +24,7 @@ function buyCake() {
     }
 }
 
+//State
 const initialState = {
     numOfCakes: 10
 }
@@ -37,3 +41,26 @@ const reducer = (state = initialState, action) => {
     }
 }
 
+
+///////    Store    ////////
+
+//We use Redux here for the store
+// 1. Define the store
+const store = createStore(reducer);
+
+// 2. Allow access to state using getState()
+console.log('Initial State ', store.getState());  // Output: Initial State  { numOfCakes: 10 }
+
+// 3. Register to the listeners [Subscribe method accepts a function soo..]
+//store.subscribe(() => console.log("Updtaed State ", store.getState()))
+const unsubsribe = store.subscribe(() => console.log("Updtaed State ", store.getState()));
+
+// 4. To update the state
+//This usually takes actions directly but since we have a action creater (buyCakes), we can use it here
+//We repeat this just to see the output now
+store.dispatch(buyCake())  // Output: Updtaed State  { numOfCakes: 9 }
+store.dispatch(buyCake())  // Output: Updtaed State  { numOfCakes: 8 }
+store.dispatch(buyCake())  // Output: Updtaed State  { numOfCakes: 7 }
+
+// 5. Unsubsribe the listener [We do it by calling the function returened by the subscribe method]
+unsubsribe();
